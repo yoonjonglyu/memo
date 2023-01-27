e2170dfd022ec272afcaa6a02a29a61ddd9f9049
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-01-27 16:13:48 +0900

    chore: ADD REACT

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-function test() {
-  const a: any = {
-    a: 'x',
-  };
-  return a;
-}
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-console.log(test());
+createRoot(document.querySelector('#app') || document.body).render(
+  <h1>test</h1>
+);
