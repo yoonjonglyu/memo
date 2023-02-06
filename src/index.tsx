3e7c44b16d034e8ebc4f316ede98e28ba00af04b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-02-06 16:29:19 +0900

    feat(APP): INIT

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-createRoot(document.querySelector('#app') || document.body).render(
-  <h1>test</h1>
-);
+import App from './App';
+// 원래 document.body에 바로 root를 잡는건 금지 됩니다. 외부에서 바로 특정 해서 접근이 가능하기에
+// 라이브러리 차원에서 경고를 표시하죠.
+// createRoot(document.querySelector('#app') || document.body).render(
+//   <App />
+// );
+const Root = document.querySelector('#app');
+if (Root !== null) {
+  createRoot(Root).render(<App />);
+} else console.error('루트 node를 찾을 수 없습니다.');
