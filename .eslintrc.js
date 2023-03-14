7de02fabe7bb8d535127c101f2eef76e4a282db8
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-03-14 21:30:59 +0900

    chore: ADD 일렉트론

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,30 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    browser: true
+    browser: true,
   },
-  extends: ['eslint:recommended', 'google', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
-  plugins: ['jsdoc', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'google',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   rules: {
+    'js-doc': false,
     'prettier/prettier': 'error',
     semi: ['error', 'always'],
     'no-implicit-coercion': 'error',
     'no-undef': 'off',
     indent: 'off',
     'no-unused-vars': 'warn',
-    'object-curly-spacing': 'off'
-  }
+    'object-curly-spacing': 'off',
+  },
 };
