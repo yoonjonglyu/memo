a089d37a16111f1c807e9a33c119b3510903f274
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-02-14 19:51:04 +0900

    chore(AND): EXPO WEB SETTING

--- packages/memo-and/__tests__/memo-and.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const memoAnd = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(memoAnd(), 'Hello from memoAnd');
-console.info('memoAnd tests passed');
--- packages/memo-and/lib/memo-and.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = memoAnd;
-
-function memoAnd() {
-  return 'Hello from memoAnd';
-}
