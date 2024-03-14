68f9ef3bcf1b4a7d9cacd327d2b451ee5dafd1d6
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-03-14 14:49:05 +0900

    fix(MOLECULES): PINPUT - EDIT & PROPS

--- packages/demo/src/components/molecules/PInput.tsx
+++ packages/demo/src/components/molecules/PInput.tsx
@@ -1,29 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Wrap = styled.div`
-margin: 50px;
+  margin: 8px;
+  border: 0.5px solid gray;
 `;
-const TextArea = styled.textarea`
-  height: 500px;
-  background-color: blue;
+const P = styled.div`
+  width: 100%;
+  min-height: 80px;
+  outline: none;
 `;
-const ViewArea = styled.div`
-  height: 500px;
-  background-color: red;
-`;
+export interface PInputProps {
+  value?: string;
+  setValue?: Function;
+}
+let prev: any = null;
 
-export interface PInputProps {}
+const PInput: React.FC<PInputProps> = ({ value, setValue }) => {
+  const [pValue, setPValue] = useState(value || '');
+  const handleType: React.KeyboardEventHandler<HTMLDivElement> = e => {
+    const target = e.target as HTMLDivElement;
+    if (prev !== null) clearTimeout(prev);
+    prev = setTimeout(() => setPValue(target.textContent || ''), 500);
+  };
 
-const PInput: React.FC<PInputProps> = () => {
-  const [isType, setIsType] = useState(false);
-  const [value, setValue] = useState('');
-  const startType = () => setIsType(true);
-  const endType = () => setIsType(false);
+  useEffect(() => {
+    if (setValue) setValue(pValue);
+  }, [pValue]);
 
   return (
-    <Wrap onClick={startType} onBlur={endType}>
-      {isType ? <TextArea value={value} /> : <ViewArea>{value}</ViewArea>}
+    <Wrap>
+      <P contentEditable="true" onKeyUp={handleType}>
+        {pValue}
+      </P>
     </Wrap>
   );
 };
