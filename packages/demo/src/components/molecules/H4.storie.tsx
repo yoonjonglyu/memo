79590396af74be1081f4a00253f3333d3f486db1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-04-18 15:00:14 +0900

    feat(MOLECULE): H4 - INIT

---
+++ packages/demo/src/components/molecules/H4.storie.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import H4 from './H4';
+
+const meta: Meta<typeof H4> = {
+  title: '메모/Ui/molecules/H4',
+  component: H4,
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    value: 'H4 타이틀 입니다.',
+    setValue: (state: string) => console.log(state),
+  },
+};
