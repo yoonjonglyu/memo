d3f37d31fe864f844f44e8e65bb5c2323859dea0
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-04-20 05:38:58 +0900

    fix(ORGANISM): NOTE - HEADER & BODY

--- packages/demo/src/components/organisms/Note.tsx
+++ packages/demo/src/components/organisms/Note.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 import MemoBox from '../atoms/MemoBox';
+import H1 from '../molecules/H1';
 import PInput from '../molecules/PInput';
+
+const Header = styled.div``;
+const Body = styled.div``;
 
 export interface NoteProps {}
 const Note: React.FC<NoteProps> = () => {
   return (
     <MemoBox>
-      <PInput />
-      <PInput />
-      <PInput />
+      <Header>
+        <H1 value="" setValue={(state: any) => console.log(state)} />
+      </Header>
+      <Body>
+        <PInput value="" setValue={(state: any) => console.log(state)} />
+      </Body>
     </MemoBox>
   );
 };
