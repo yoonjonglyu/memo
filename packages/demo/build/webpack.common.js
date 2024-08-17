1c323169bbbc7b1b0f3f17cd8544b33f13d17c1c
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-08-17 21:03:42 +0900

    chore(DEMO): SETTING WEBPACK WORKBOX

--- packages/demo/build/webpack.common.js
+++ packages/demo/build/webpack.common.js
@@ -1,6 +1,7 @@
 const commonPaths = require('./common-paths');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const config = {
   entry: {},
@@ -33,6 +34,11 @@ const config = {
         appShortName: 'Memo',
         appDescription: 'crossplatform basic Planner Memo App',
       },
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000
     }),
   ],
 };
