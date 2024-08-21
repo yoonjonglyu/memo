1226f17a4c7eb24051c7885aca84f8ab5ac49123
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-08-21 17:41:27 +0900

    fix(API): INIT DATA CONVERT

--- packages/demo/src/api/memoApi.ts
+++ packages/demo/src/api/memoApi.ts
@@ -20,13 +20,13 @@ class MemoApi {
       const check = localStorage.getItem('MEMO_LIST');
       if (check === null) {
         const newData = [
-          { idx: 'a1', type: 'memo', props: '여기다 메모를 작성해보세요.' },
+          { idx: 'a1', type: 'memo', props: 'write memo.' },
           {
             idx: 'a2',
             type: 'todo',
             props: [
-              { isAvail: true, todo: '메모장을 켜기' },
-              { isAvail: false, todo: '새로운 메모를 작성해보세요.' },
+              { isAvail: true, todo: 'open memo' },
+              { isAvail: false, todo: 'add new todo or memo' },
             ],
           },
         ];
