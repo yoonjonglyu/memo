8a5e3b4c3dd8fa8b866f776121dde24e7a53f9ab
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-09-01 21:02:45 +0900

    feat(HOOK): USE IMPORT - JSON ACCEPT & ERR

--- packages/demo/src/hooks/useImport.ts
+++ packages/demo/src/hooks/useImport.ts
@@ -11,6 +11,7 @@ function useImport() {
     const reader = new FileReader();
     const input = document.createElement('input');
     input.setAttribute('type', 'file');
+    input.setAttribute('accept', '.json');
 
     input.onchange = e => {
       const target = e.target as HTMLInputElement;
@@ -19,9 +20,13 @@ function useImport() {
 
       reader.onloadend = async () => {
         if (isString(reader.result)) {
-          const data = JSON.parse(reader.result);
-          if (isArray(data)) await MemoSignal.setMemoList(data);
-          await initMemo();
+          try {
+            const data = JSON.parse(reader.result);
+            if (isArray(data)) await MemoSignal.setMemoList(data);
+            await initMemo();
+          } catch (err) {
+            console.error('Invalid JSON file', err);
+          }
         }
       };
     };
