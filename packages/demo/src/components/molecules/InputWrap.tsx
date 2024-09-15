703e653b4a426945ac3b5571645c6b096e185268
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-09-15 18:57:40 +0900

    feat(MOLECULE): INPUT WRAP - MENU UI

--- packages/demo/src/components/molecules/InputWrap.tsx
+++ packages/demo/src/components/molecules/InputWrap.tsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import SquareButton, { SquareButtonProps } from './SquareButton';
 
 const Wrap = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
+  flex-flow: row nowrap;
 `;
 const HandleBox = styled.div`
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: row nowrap;
   align-items: center;
   width: auto !important;
   margin-left: 2px;
+  gap: 2px;
 `;
 
 export interface InputWrapProps {
   children?: React.ReactNode;
-  SquareButtonProps?: SquareButtonProps;
+  addButtonProps?: SquareButtonProps;
+  removeButtonProps?: SquareButtonProps;
 }
 
 const InputWrap: React.FC<InputWrapProps> = ({
   children,
-  SquareButtonProps,
+  addButtonProps,
+  removeButtonProps,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsOpen(prev => !prev);
+  };
+
   return (
     <Wrap>
       {children}
       <HandleBox>
-        <SquareButton iconType="menu" />
-        <SquareButton iconType="add" />
-        <SquareButton iconType="remove" {...SquareButtonProps} />
+        <SquareButton iconType="menu" onClick={handleMenuOpen} />
+        {isOpen ? <SquareButton iconType="add" {...addButtonProps} /> : null}
+        {isOpen ? (
+          <SquareButton iconType="remove" {...removeButtonProps} />
+        ) : null}
       </HandleBox>
     </Wrap>
   );
