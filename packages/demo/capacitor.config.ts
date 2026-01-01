d6d38c277d797324aa703f3069249074aa94cb47
Isa(yunjongryu) <yunjonglyu@gmail.com>
2026-01-01 23:01:24 +0900

    chore(DEMO): ADD CAP

---
+++ packages/demo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yoonjongryu.memo',
+  appName: 'memo',
+  webDir: 'dist'
+};
+
+export default config;
