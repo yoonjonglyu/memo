425ccb3ecae8f2e519b0768c5a3a82a951bca10e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2026-01-06 13:19:43 +0900

    chore: CAPACITOR CONFIG UPDATE

--- packages/demo/capacitor.config.ts
+++ packages/demo/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.yoonjongryu.memo',
-  appName: 'memo',
+  appName: 'MemoFlow',
   webDir: 'dist'
 };
 
